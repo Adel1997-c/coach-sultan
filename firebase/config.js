5d6d66d6209a45dd200711c54cc15cae3669dfaf
Luca A. <aadelalmashi1@gmail.com>
2025-07-26 22:27:09 +0300

    fix: connect Firebase config with env variables

--- firebase/config.js
+++ firebase/config.js
@@ -1,22 +1,20 @@
-// firebase/firebaseConfig.js
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-import { getStorage } from "firebase/storage";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC9zrzUSrKA5tPjRBmgRfKSs1-eqQJYc3Q",
-  authDomain: "couch-me.firebaseapp.com",
-  projectId: "couch-me",
-  storageBucket: "couch-me.appspot.com",
-  messagingSenderId: "582714505013",
-  appId: "1:582714505013:web:1fcf3b20e735055f753e7c",
-  measurementId: "G-LZSKN6QYT6"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth(app);
-const storage = getStorage(app);
 
-export { db, auth, storage };
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
